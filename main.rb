20dbf368b34cb87bad7022cf9d3d665f0d298232
hiromu-kon <hiro.0405soccer@icloud.com>
2021-06-23 21:52:58 +0900

    モンスターの攻撃処理実装

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ class Brave
   attr_reader :name, :offense, :defense
   attr_accessor :hp
 
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
@@ -15,25 +17,73 @@ class Brave
   end
 
   def attack(monster)
-    damage = @offense - monster.defense
-    monster.hp -= damage
-
     puts "#{@name}の攻撃"
+    attack_num = rand(4)
+
+    if attack_num == 0
+      puts "必殺攻撃"
+      damage = calculate_special_attack - monster.defense
+    else
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
+
+    monster.hp -= damage
+    
     puts "#{monster.name}は#{damage}のダメージを受けた"
     puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+  def calculate_special_attack
+    @offense * SPECIAL_ATTACK_CONSTANT
   end
 end
 
 class Monster
-  attr_reader :name, :offense, :defense
-  attr_accessor :hp
+  attr_reader :offense, :defense
+  attr_accessor :hp, :name
 
-  def initialize(params)
+  POWER_UP_RATE = 1.5
+  CALC_HALF_HP = 0.5
+
+  def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_HP
   end
+
+  def attack(brave)
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+    end
+
+    puts "#{@name}の攻撃"
+    damage = @offense - brave.defense
+  
+    brave.hp -= damage
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+
+    def transform
+      transform_name = "ドラゴン"
+
+      puts <<~EOS
+      #{@name}は怒っている
+      #{@name}は#{transform_name}に変身した
+      EOS
+
+      @offense *= POWER_UP_RATE
+      @name = transform_name
+    end
 end
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
@@ -43,3 +93,4 @@ monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 
 brave.attack(monster)
+monster.attack(brave)
